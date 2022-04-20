eab762cd1a753d747fa03b8ed109d053383d12aa
Afolabi Peace <peaceafolabio4@gmail.com>
2022-04-20 09:06:38 +0100

    I made sign in possible

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,6 +1,6 @@
 function here(){
     local = JSON.parse(localStorage.getItem('Bankapp'));
-local2 = JSON.parse(localStorage.getItem('signin'))
+local2 = JSON.parse(localStorage.getItem("signin"))
 wel.innerText ="Welcome,"+" "+ local2.firstName +" "+" "+ local2.secondName + " "+"Your Balance is,"+local2.balance;
 acct.innerText = "Your Account Number is,"+" "+ local2.accountNum
 var bal = 5000;
--- javascript/signup.js
+++ javascript/signup.js
@@ -2,36 +2,43 @@ var allRegister = [];
 var bals = [];
 var bal = 5000;
 bals.push(bal)
-var acctNo = Math.floor(Math.random()*10000000000)
-if(localStorage.getItem("Bankapp")){
-    allRegister=JSON.parse(localStorage.getItem("Bankapp"))
+var acctNo = Math.floor(Math.random() * 10000000000)
+if (localStorage.getItem("Bankapp")) {
+    allRegister = JSON.parse(localStorage.getItem("Bankapp"))
 }
-function creatHere(){
-if(fn.value==""&&email.value==""&&pass.value==""&&sn.value==""){
-    alert('Pleace enter your details')
-}
-else if(pass.value != cpass.value){
-    alert('check your password')
-}
-else{
-    register={};
-register.firstName = fn.value;
-register.secondName = sn.value;
-register.cp = cpass.value;
-register.password = pass.value;
-register.email = email.value;
-register.state = sta.value;
-register.country = con.value;
-register.add = address.value;
-register.dobb = dob.value;
-register.balance = bals;
-register.accountNum = acctNo;
-allRegister.push(register)
-console.log(allRegister)
-localStorage.setItem("Bankapp",JSON.stringify(allRegister))
-alert('Welcome' + " " + register.firstName + " "+register.secondName+" "+register.email)
-alert(acctNo)
-// location.reload();
-}
+function creatHere() 
+{
+    if (fn.value == "" && email.value == "" && pass.value == "" && sn.value == ""){
+        alert('Pleace enter your details')
+    }
+    else {
+         if (pass.value != cpass.value) {
+            alert('check your password')
+        }
+        // else if(allRegister.find((val) => val.email == email.value)
+        // {
+        //     alert('Email already exist')
+        // }
+        else{
+        register = {};
+        register.firstName = fn.value;
+        register.secondName = sn.value;
+        register.cp = cpass.value;
+        register.password = pass.value;
+        register.email = email.value;
+        register.state = sta.value;
+        register.country = con.value;
+        register.add = address.value;
+        register.dobb = dob.value;
+        register.balance = bals;
+        register.accountNum = acctNo;
+        allRegister.push(register)
+        console.log(allRegister)
+        localStorage.setItem("Bankapp", JSON.stringify(allRegister))
+        alert('Welcome' + " " + register.firstName + " " + register.secondName + " " + register.email)
+        alert(acctNo)
+        // location.reload();
+        }
+    }
 
 }   
